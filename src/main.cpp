527d95a9f973ad73c76fdfcb6bbd0c56fad63991
ruwix <grantpauker@gmail.com>
2019-02-15 19:27:40 -0800

    Add simple pwm code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "pid.hpp"
+#include "pca9685.hpp"
 
 int main()
 {
-    PID controller = PID(1, 0, 0);
-    float output = controller.calculate(10, 0, 0);
+    PCA9685 pwmdriver(50);
+    pwmdriver.setPWM(0, 1024, 3072);
+    for (int pin = 0; pin < 16; pin++)
+    {
+        printf("PWM Pin %d: %d", pin, pwmdriver.getPWM(pin));
+    }
     return 0;
 }
