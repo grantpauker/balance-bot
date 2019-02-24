5ce1bb1d744e8e79ce61c6075c0e3a25ce1bfb18
ruwix <grantpauker@gmail.com>
2019-02-23 21:24:39 -0800

    Add bno055 test to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
+#include <assert.h>
 #include "pid.hpp"
 #include "pca9685.hpp"
+#include "bno055.hpp"
+
+#define LOG(a, b) std::cout << a << ": " << b << "\n"
 
 int main()
 {
-    PCA9685 pwmdriver(50);
-    pwmdriver.setPWM(0, 1024, 3072);
-    for (int pin = 0; pin < 16; pin++)
+    BNO055 imu;
+    assert(imu.init(OPERATION_MODE_NDOF));
+    auto status = imu.getSystemStatus(true);
+    LOG("system status", status[0]);
+    LOG("self test result", status[0]);
+    if (status[0] == 0x01)
     {
-        printf("PWM Pin %d: %d/%d", pin, pwmdriver.getPWMOn(pin), pwmdriver.getPWMOff(pin));
+        LOG("system error", status[2]);
     }
-    return 0;
+    auto rev = imu.getRevision();
+    LOG("software version", rev[0]);
+    LOG("bootloader version", rev[1]);
+    LOG("accelerometer id", rev[2]);
+    LOG("magnetometer id", rev[3]);
+    LOG("gyroscope id", rev[4]);
+    while (1)
+    {
+        auto euler = imu.readEuler();
+        auto cal_status = imu.getCalibrationStatus();
+        LOG("yaw", euler[0]);
+        LOG("pitch", euler[1]);
+        LOG("roll", euler[2]);
+        LOG("sys calibration", cal_status[0]);
+        LOG("gyro calibration", cal_status[1]);
+        LOG("accel calibration", cal_status[2]);
+        LOG("mag calibration", cal_status[3]);
+    }
 }
