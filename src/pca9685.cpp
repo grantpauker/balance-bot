bb31e23a4f5b66c25a42d9f5f99166e4873558e4
ruwix <grantpauker@gmail.com>
2019-02-25 20:48:48 -0800

    Replace int pointers with std::array, add i2c to constructor

--- src/pca9685.cpp
+++ src/pca9685.cpp
@@ -1,62 +1,73 @@
 #include <iostream>
-#include <wiringPi.h>
-#include <wiringPiI2C.h>
-
+#include <array>
+#include <algorithm>
 #include "pca9685.hpp"
+#include "i2c.hpp"
 
-PCA9685::PCA9685(float freq)
+PCA9685::PCA9685(I2C i2c, float freq) : i2c(i2c)
 {
-  fd = wiringPiI2CSetup(PCA9685_I2C_ADDRESS);
-  if (fd > 0)
-  {
-    std::cout << "Error: fd = " << fd << "\n";
-  }
-  if (freq > 0)
-  {
-    setPWMFreq(freq);
-  }
+    i2c.init(PCA9685_I2C_ADDRESS);
+    if (freq > 0)
+    {
+        setPWMFreq(freq);
+    }
 }
 
 void PCA9685::setPWMFreq(int freq)
 {
-  freq = (freq > 1000 ? 1000 : (freq < 40 ? 40 : freq));
+    freq = (freq > 1000 ? 1000 : (freq < 40 ? 40 : freq));
 
-  int prescale = (int)(25000000.0f / (4096 * freq) - 0.5f);
+    int prescale = (int)(25000000.0f / (4096 * freq) - 0.5f);
 
-  int settings = wiringPiI2CReadReg8(fd, PCA9685_MODE1) & 0x7F;
-  int sleep = settings | 0x10;
-  int wake = settings & 0xEF;
-  int restart = wake | 0x80;
+    int settings = i2c.readByte(PCA9685_MODE1) & 0x7F;
+    int sleep = settings | 0x10;
+    int wake = settings & 0xEF;
+    int restart = wake | 0x80;
 
-  wiringPiI2CWriteReg8(fd, PCA9685_MODE1, sleep);
-  wiringPiI2CWriteReg8(fd, PCA9685_PRESCALE, prescale);
-  wiringPiI2CWriteReg8(fd, PCA9685_MODE1, wake);
+    i2c.writeByte(PCA9685_MODE1, sleep);
+    i2c.writeByte(PCA9685_PRESCALE, prescale);
+    i2c.writeByte(PCA9685_MODE1, wake);
 
-  delay(5);
-  wiringPiI2CWriteReg8(fd, PCA9685_MODE1, restart);
+    i2c.delay(5);
+    i2c.writeByte(PCA9685_MODE1, restart);
 }
 
 void PCA9685::setPWM(int pin, int on, int off)
 {
-  int reg = PIN(pin);
-  wiringPiI2CWriteReg16(fd, reg, on & 0x0FFF);
-  wiringPiI2CWriteReg16(fd, reg + 2, off & 0x0FFF);
+    int reg = PIN(pin);
+    i2c.writeByte(reg, on & 0x0FFF);
+    i2c.writeByte(reg + 2, off & 0x0FFF);
 }
 
-void PCA9685::setPWM(int on, int off)
+void PCA9685::setPWM(int pin, int val)
 {
-  wiringPiI2CWriteReg16(fd, PCA9685_ALL_LED_ON_L, on & 0x0FFF);
-  wiringPiI2CWriteReg16(fd, PCA9685_ALL_LED_ON_L + 2, off & 0x0FFF);
+    int reg = PIN(pin);
+    val = std::min(val, 4095);
+    if (val == 0)
+    {
+        setPWM(pin, 0, 4096);
+    }
+    else if (val == 4095)
+    {
+        setPWM(pin, 4096, 0);
+    }
+    else
+    {
+        setPWM(pin, 0, val);
+    }
 }
 
-void PCA9685::getPWM(int pin, int *on, int *off)
+std::array<int, 2> PCA9685::getPWM(int pin)
 {
-  int reg = PIN(pin);
-  *on = wiringPiI2CReadReg16(fd, reg);
-  *off = wiringPiI2CReadReg16(fd, reg + 2);
+    int reg = PIN(pin);
+    std::array<int, 2> ret;
+    ret[0] = i2c.readByte(reg);
+    ret[1] = i2c.readByte(reg + 2);
+    return ret;
 }
+
 void PCA9685::reset()
 {
-  wiringPiI2CWriteReg16(fd, PCA9685_ALL_LED_ON_L, 0x0);
-  wiringPiI2CWriteReg16(fd, PCA9685_ALL_LED_ON_L + 2, 0x1000);
+    i2c.writeByte(PCA9685_ALL_LED_ON_L, 0x0);
+    i2c.writeByte(PCA9685_ALL_LED_ON_L + 2, 0x1000);
 }
--- src/pca9685.hpp
+++ src/pca9685.hpp
@@ -1,26 +1,29 @@
 #ifndef H_PCA9685
 #define H_PCA9685
 
+#include <array>
+#include "i2c.hpp"
+
 #define PCA9685_I2C_ADDRESS 0x40
 #define PCA9685_MODE1 0x00
 #define PCA9685_PRESCALE 0xFE
 #define PCA9685_LED0_ON_L 0x6
 #define PCA9685_ALL_LED_ON_L 0xFA
 
-#define PIN(p) PCA9685_LED0_ON_L + 4 * p
+#define PIN(p) p != 16 ? PCA9685_LED0_ON_L + 4 * p : PCA9685_ALL_LED_ON_L
 
 class PCA9685
 {
 public:
-  PCA9685(float);
+  PCA9685(I2C, float);
   void setPWMFreq(int);
   void setPWM(int, int, int);
   void setPWM(int, int);
-  void getPWM(int, int *, int *);
+  std::array<int, 2> getPWM(int);
 
   void reset();
 
 private:
-  int fd;
+  I2C i2c;
 };
 #endif
