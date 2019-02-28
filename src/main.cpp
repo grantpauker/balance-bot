6493f0ae5ed150c4a57f651205ac9fe4883f14d8
ruwix <grantpauker@gmail.com>
2019-02-27 20:13:15 -0800

    Fix motor pins, i2c usae, and use motor controller class

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,32 @@
 #include "bno055.hpp"
 #include "i2c.hpp"
 #include "l298n.hpp"
+#include "motorcontroller.hpp"
 
 #define LOG(a, b) std::cout << a << ": " << b
 #define LOGN(a, b) std::cout << a << ": " << b << "\n"
 
-#define I2C_BUS_1 1
-#define I2C_BUS_2 1
+#define I2C_BUS 1
 #define PWM_FREQ 50
 
-#define L_MOTOR_IN1 1
+#define L_MOTOR_IN1 0
 #define L_MOTOR_IN2 2
-#define L_MOTOR_ENABLE 3
+#define L_MOTOR_ENABLE 0
 
-#define R_MOTOR_IN1 4
-#define R_MOTOR_IN2 5
-#define R_MOTOR_ENABLE 6
+#define R_MOTOR_IN1 15
+#define R_MOTOR_IN2 16
+#define R_MOTOR_ENABLE 1
 
-#define KP 1
-#define KI 0.001
+#define KP 45.5
+#define KI 0.0
 #define KD 0.0
 #define DT 2
 
 int main()
 {
-    I2C i2c1(I2C_BUS_1);
-    I2C i2c2(I2C_BUS_2);
-    BNO055 imu(i2c1);
+    I2C i2c(I2C_BUS);
+    i2c.enable(BNO055_ADDRESS_A);
+    BNO055 imu(i2c);
     assert(imu.init(OPERATION_MODE_NDOF));
     auto status = imu.getSystemStatus(true);
     LOGN("imu system status", status[0]);
@@ -39,15 +39,20 @@ int main()
     {
         LOGN("imu system error", status[2]);
     }
-    PCA9685 pwm_driver(i2c2, PWM_FREQ);
-    L298N left_motor(pwm_driver, L_MOTOR_ENABLE, L_MOTOR_IN1, L_MOTOR_IN2);
-    L298N right_motor(pwm_driver, R_MOTOR_ENABLE, R_MOTOR_IN1, R_MOTOR_IN2);
-    PID pid(KP, KI, KD);
-
+    PCA9685 pwm_driver(i2c, PWM_FREQ);
+    L298N left_l298n(pwm_driver, L_MOTOR_ENABLE, L_MOTOR_IN1, L_MOTOR_IN2);
+    L298N right_l298n(pwm_driver, R_MOTOR_ENABLE, R_MOTOR_IN1, R_MOTOR_IN2);
+    MotorController left_motor(left_l298n);
+    MotorController right_motor(right_l298n);
+    PID pid(KP, KI, KD, -4096, 4096);
+    float output;
+    float pitch;
     while (1)
     {
-        float pitch = imu.readEuler()[1];
-        float output = pid.calculate(DT, 0, pitch);
+        i2c.enable(BNO055_ADDRESS_A);
+        pitch = imu.readEuler()[1];
+        output = pid.calculate(DT, 0, pitch);
+        i2c.enable(PCA9685_ADDRESS);
         left_motor.setSpeed(output);
         right_motor.setSpeed(output);
         std::cout << pitch << "\t" << output << "\n";
